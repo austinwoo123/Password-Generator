9059dd5b221a1101ae856ab0e5a238e7b11e18fc
Austin W <austinwoo123@gmail.com>
2020-12-17 23:01:32 -0800

    create an if and else if statement with the first prompt that the user will receive when visiting the site

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //Possible strings//
-var uppperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
-var specials = "!@#$%^&*_?"
+var specials = "!@#$%^&*_?";
 
+//console.log(upperCase)
+//console.log(lowerCase)
+//console.log(numbers)
+//console.log(specials)
+
+// new variable to track the confirms from the prompts//
+var newPassword = [];
+
+//Prompts and confirms//
+var passwordLength = parseInt(prompt("How many characters do you want in your password?"));
+
+if (passwordLength < 8) {
+  alert("Your password needs to have at least 8-128 characters.");
+  prompt("How many characters do you want in your password?");
+
+}
+else if (passwordLength > 8); {
+
+}
 
 
 // Write password to the #password input
